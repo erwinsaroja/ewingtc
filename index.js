08999a0791837cdeb3b880b4a102bffc7e6c1965
erwinsaroja <56242794+erwinsaroja@users.noreply.github.com>
2019-10-07 10:38:27 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+var no1=document.uts.item1.value;
+var no2=document.uts.item2.value;
+var no3=document.uts.item3.value;
+
+var nilaiangka=0;
+var nilaihuruf="";
+
+function nilai() {
+    if (no1==="item1a") {
+        nilaiangka+=3;
+    }
+    
+     if (no2==="item2b") {
+        nilaiangka+=3;
+    }
+    
+     if (no3==="item3c") {
+        nilaiangka+=3;
+    }
+    
+    if (nilaiangka>=7 && nilaiangka<=9) {
+        nilaihuruf="A";
+    }
+    
+    if (nilaiangka>=4 && nilaiangka<=6) {
+        nilaihuruf="B";
+    }
+    
+    if (nilaiangka>=0 && nilaiangka<=3) {
+        nilaihuruf="C";
+    }
+    
+    document.uts.nilai.value=nilaihuruf +" " + "("+nilaiangka+")";
+    
+     
+    
+}
+
+
+
